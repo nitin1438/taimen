b211876bb4e4a37a27a9c4152a40ddc82c14618f
nitin1438 <nitin.chobhe@gmail.com>
2018-01-18 14:33:42 +0000

    Make kernel permissive and add toolchain path

--- BoardConfigCarbon.mk
+++ BoardConfigCarbon.mk
@@ -6,5 +6,11 @@ TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_NEEDS_DTBOIMAGE := true
 TARGET_COMPILE_WITH_MSM_KERNEL := true
 
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-8.0/bin
+KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
+
+# Snapdragon LLVM
+#TARGET_USE_SDCLANG := true
+
 # Sepolicy
 BOARD_SEPOLICY_DIRS += device/google/marlin/sepolicy/verizon
